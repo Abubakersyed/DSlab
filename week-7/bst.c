bdf2c829a5a08b8d90bc1456a976ce27f05e363a
Abubakersyed <abubakersyed03@gmail.com>
2025-11-11 22:59:52 +0530

    Update bst.c

--- week-7/bst.c
+++ week-7/bst.c
@@ -28,34 +28,12 @@ struct Node* insertElement(struct Node* root,int key)
 	return root;
 	
 }
-void InorderTraversal(struct Node* root)
+struct Node* minValueNode(struct Node* node)
 {
-	if(root)
-	{
-		inorderTraversal(root->left);
-		printf("%d ",root->key);
-		inorderTraversal(root->right);
-		
-	}
-}
-void PreorderTraversal(struct Node* root)
-{
-	if(root)
-	{
-		printf("%d ",root->key);
-		PreorderTraversal(root->left);
-		PreorderTraversal(root->right);
-	}
-}
-
-void Postordertraversal(struct Node* root)
-{
-	if(root)
-	{
-		Postordertraversal(root->left);
-		Postordertraversal(root->right);
-		printf("%d ",root->key);
-	}
+struct Node* current = node;
+while (current && current->left != NULL)
+current = current->left;
+return current;
 }
 
 struct Node* deleteNode(struct Node*, int key)
@@ -67,12 +45,15 @@ struct Node* deleteNode(struct Node*, int key)
 	root->right=deleteNode(root->right,key);
 	else
 	{
+		// no child
 		if(root->left==NULL && root->right==NULL)
 		{
 			free(root);
 			return NULL;
 		}
-	
+
+			
+	// one child
 	else if(root->left==NULL)
 	{
 		struct Node* temp = root->right;
@@ -85,6 +66,7 @@ struct Node* deleteNode(struct Node*, int key)
 		free(root);
 		return NULL;
 	}
+		// two children
 	struct Node* temp = MinvalueNode(root->right);
 	root->key=temp->key;
 	root->right = deleteNode(root->right,temp->key);
@@ -92,7 +74,73 @@ struct Node* deleteNode(struct Node*, int key)
 		return root;
 }
 
+// Search for an element in BST
+struct Node* searchElement(struct Node* root, int key)
+{
+if (root == NULL || root->key == key)
+return root;
+if (key < root->key)
+return searchElement(root->left, key);
+else
+return searchElement(root->right, key);
+}
 
+// Display inorder traversal (to check tree contents)
+void inorderTraversal(struct Node* root)
+{
+if (root)
+{
+inorderTraversal(root->left);
+printf("%d ", root->key);
+inorderTraversal(root->right);
+}
+}
+int main()
+{
+struct Node* tree = NULL;
+struct Node* ptr;
+int choice, val;
+while (1)
+{
+printf("\n--- Binary Search Tree Menu ---\n");
+printf("1. Insert\n2. Delete\n3. Search\n4. Display (Inorder)\n5.Exit\n");
+printf("Enter choice: ");
+scanf("%d", &choice);
 
+switch (choice)
+
+{
+case 1:
+printf("\nEnter the value to insert: ");
+scanf("%d", &val);
+tree = insertElement(tree, val);
+break;
+case 2:
+printf("\nEnter the value to delete: ");
+scanf("%d", &val);
+tree = deleteNode(tree, val);
+break;
+case 3:
+printf("\nEnter the value to search: ");
+scanf("%d", &val);
+ptr = searchElement(tree, val);
+if (ptr)
+printf("\nElement %d found in the tree.", val);
+else
+printf("\nElement %d not found in the tree.", val);
+break;
+case 4:
+printf("\nInorder Traversal: ");
+inorderTraversal(tree);
+printf("\n");
+break;
+case 5:
+exit(0);
+default:
+printf("\nInvalid choice!");
+}
+}
+return 0;
+}
 
 
